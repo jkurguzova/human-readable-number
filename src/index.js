097a99a89d0285059f1cf27d3cd92c97c43c6aee
jkurguzova <jkurguzova@inbox.ru>
2020-10-14 15:21:39 +0300

    feat: implement readability

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let numStr = num.toString();
+  if (num == 0) {
+      return 'zero';
+  } else if (num < 20) {
+      return ones[num];
+  } else if (numStr.length == 2 && numStr[1] === '0') {
+      return tens[numStr[0]];
+  } else if (numStr.length == 2 && numStr[1] !== '0') {
+      return tens[numStr[0]]+' '+ ones[numStr[1]];
+  } else if (numStr.length == 3) {
+      if (numStr[1] === '0' && numStr[2] === '0') {
+        return ones[numStr[0]] + ' hundred';
+      } else {
+      return ones[numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]));
+    }
+  }
 }
+
